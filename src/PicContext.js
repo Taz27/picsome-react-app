8070281f29d482359269c934d5a1b7c61c08b388
Taran Mand <tsmand@yahoo.com>
2020-01-29 13:35:04 +1100

    Add context state

--- src/PicContext.js
+++ src/PicContext.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 const PicContext = React.createContext();
 
 function PicContextProvider(props) {
+    const [allPhotos, setAllPhotos] = useState([]);
+
     return (
-        <PicContext.Provider value={""}>
+        <PicContext.Provider value={{allPhotos, setAllPhotos}}>
             {props.children}
         </PicContext.Provider>
     );
