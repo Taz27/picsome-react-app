3aab7b4b70b02cf3c64ffd0e6ed788d691f758d7
Taran Mand <tsmand@yahoo.com>
2020-01-29 14:44:22 +1100

    Add fetch to get data from API

--- src/PicContext.js
+++ src/PicContext.js
@@ -1,8 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 const PicContext = React.createContext();
 
 function PicContextProvider(props) {
     const [allPhotos, setAllPhotos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
+            .then(response => response.json())
+            .then(data => setAllPhotos(data))
+    }, []);
 
     return (
         <PicContext.Provider value={{allPhotos, setAllPhotos}}>
