9ff42cbe3df2dc6c51370e71b3fe2387eff2ee78
Taran Mand <tsmand@yahoo.com>
2020-01-30 21:04:54 +1100

    Add localStorage, img propTypes, add Items to Cart

--- src/PicContext.js
+++ src/PicContext.js
@@ -4,6 +4,7 @@ const PicContext = React.createContext();
 function PicContextProvider(props) {
     const [allPhotos, setAllPhotos] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
 
     function toggleFavorite(imgId) {
         //console.log(imgId);
@@ -14,10 +15,47 @@ function PicContextProvider(props) {
             return pic;
         });
 
+        if (typeof(Storage) !== "undefined") {
+            localStorage.setItem("photosArray", JSON.stringify(newImgArray));
+        }
+
         setAllPhotos(newImgArray);
     }
+
+    function addToCart(img) {
+        let found = false;
+        for(let i = 0; i < cartItems.length; i++) {
+            if (cartItems[i].id === img.id) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            setCartItems(prevCart => [...prevCart, img]);
+        }
+        
+    }
+    //console.log(JSON.stringify(cartItems));
+    
+    // useEffect(() => {
+    //     //get all the favorite photos in a new array
+    //     let favPhotos = allPhotos.filter(pic => pic.isFavorite);
+    //     //console.log("fav pics: " + JSON.stringify(favPhotos));
+        
+    //     //Store Favorited Photos in session
+    //     localStorage.setItem("favoritePics", JSON.stringify(favPhotos));
+    // }, [allPhotos]);
     
     useEffect(() => {
+        if (typeof(Storage) !== "undefined") {
+            if (localStorage.photosArray) {
+                let picArray = JSON.parse(localStorage.photosArray);
+                setAllPhotos(picArray);
+                return;
+            }
+        }
+        
         setIsLoading(true);
 
         fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
@@ -29,7 +67,7 @@ function PicContextProvider(props) {
     }, []);
 
     return (
-        <PicContext.Provider value={{allPhotos, isLoading, toggleFavorite}}>
+        <PicContext.Provider value={{allPhotos, isLoading, toggleFavorite, addToCart}}>
             {props.children}
         </PicContext.Provider>
     );
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,12 @@
 import React, {useState, useContext} from "react";
 import {PicContext} from "../PicContext";
+import PropTypes from "prop-types";
 
 function Image(props) {
     const [hovered, setHovered] = useState(false);
 
     const {toggleFavorite} = useContext(PicContext);
+    const {addToCart} = useContext(PicContext);
     
     return (
         <div className={`${props.className} image-container`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
@@ -12,11 +14,20 @@ function Image(props) {
             {hovered ? 
             <>
                 <i className={props.img.isFavorite ? "ri-heart-fill favorite" : "ri-heart-line favorite"} onClick={() => toggleFavorite(props.img.id)}></i>
-                <i className="ri-add-circle-line cart"></i>
+                <i className="ri-add-circle-line cart" onClick={() => addToCart(props.img)}></i>
             </> :
             props.img.isFavorite ? <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(props.img.id)}></i> : undefined}
         </div>
     );
 }
+
+Image.propTypes = {
+    className: PropTypes.string,
+    img: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool
+    })
+};
 
 export default Image
